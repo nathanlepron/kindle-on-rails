be45f17da5045fb098f0f88d560e67b8a5f0c6e5
Nathan Lepron <48675531+nathanlepron@users.noreply.github.com>
2024-05-15 14:29:11 +0200

    Create Application Layout with Connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
-end
+    protect_from_forgery with: :exception
+    def not_found_method
+      render template: 'shared/404', status: :not_found, layout: 'layouts/application'
+    end
+    private
+  
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  
+    def logged_in?
+      !!current_user
+    end
+
+    helper_method :current_user, :logged_in?
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :books
+  root 'home#index'
+  resources :registrations
+  resources :sessions, only: [:new, :create]
+  match '*unmatched', to: 'application#not_found_method', via: :all
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
